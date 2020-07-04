75d33bf17bbc47b2a16205505c653b8567f77f79
Fábio Ferreira <martinhoferreira10@gmail.com>
2020-07-04 09:35:45 +0100

    Fix date time formatter to always have two digits on hours, minutes and seconds.

--- client/src/formatters/formatDateTime.ts
+++ client/src/formatters/formatDateTime.ts
@@ -3,12 +3,16 @@ export default function (date: number | string) {
   let month = `${d.getUTCMonth() + 1}`;
   let day = `${d.getUTCDate()}`;
   const year = d.getUTCFullYear();
-  const hours = d.getUTCHours();
-  const minutes = d.getUTCMinutes();
-  const seconds = d.getUTCSeconds();
+  let hours = `${d.getUTCHours()}`;
+  let minutes = `${d.getUTCMinutes()}`;
+  let seconds = `${d.getUTCSeconds()}`;
 
   if (month.length < 2) { month = `0${month}`; }
   if (day.length < 2) { day = `0${day}`; }
+  if (hours.length < 2) { hours = `0${hours}`; }
+  if (minutes.length < 2) { minutes = `0${minutes}`; }
+  if (seconds.length < 2) { seconds = `0${seconds}`; }
+
 
   return `${[year, month, day].join('-')}T${[hours, minutes, seconds].join(':')}`;
 }
